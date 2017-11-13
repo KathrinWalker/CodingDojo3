7a34cab79d2346f2179a32cd6b53a09dc1f7536b
Kathrin Walker <Kathrin Walker@DESKTOP-KL30K26>
2017-11-13 17:40:53 +0100

    String To Brush Converter

---
+++ CodingDojo3/Converter/StringToBrushConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CodingDojo3.Converter
+{
+    public class StringToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string temp = (string)value;
+            if (temp == "Enabled") return new SolidColorBrush(Colors.Green);
+            else if(temp == "Disabled") return new SolidColorBrush(Colors.Red);
+            else if (temp == "Auto") return new SolidColorBrush(Colors.Yellow);
+            return new SolidColorBrush(Colors.Blue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CodingDojo3/ViewModel/MainViewModel.cs
+++ CodingDojo3/ViewModel/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System;
 using System.Collections.Generic;
 using Shared.Interfaces;
+using Shared.BaseModels;
 
 namespace CodingDojo3.ViewModel
 {
@@ -13,15 +14,18 @@ namespace CodingDojo3.ViewModel
         private Simulator sim;
         private List<ItemVm> Data { get; set; }
 
-        public ObservableCollection<ItemVm> SensorList {get; set; }
+        public ObservableCollection<ItemVm> SensorList { get; set; }
 
         public ObservableCollection<ItemVm> ActorList { get; set; }
+
+        public ObservableCollection<string> ModeList { get; set; }
 
         public MainViewModel()
         {
             Data = new List<ItemVm>();
             SensorList = new ObservableCollection<ItemVm>();
             ActorList = new ObservableCollection<ItemVm>();
+            ModeList = new ObservableCollection<string>();
             GetData();
         }
 
@@ -30,11 +34,24 @@ namespace CodingDojo3.ViewModel
             Simulator sim = new Simulator(Data);
             foreach (var item in sim.Items)
             {
-                if (item.ItemType.Equals(typeof(ISensor))){
+                if (item.ItemType.Equals(typeof(ISensor)))
+                {
                     SensorList.Add(item);
-                }else if (item.ItemType.Equals(typeof(IActuator))){
+                }
+                else if (item.ItemType.Equals(typeof(IActuator)))
+                {
                     ActorList.Add(item);
                 }
+            }
+
+            foreach (var item in Enum.GetNames(typeof(SensorModeType)))
+            {
+                ModeList.Add(item);
+            }
+
+            foreach (var item in Enum.GetNames(typeof(ModeType)))
+            {
+                ModeList.Add(item);
             }
         }
     }
